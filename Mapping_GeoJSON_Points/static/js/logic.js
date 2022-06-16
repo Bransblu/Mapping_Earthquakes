aab13e1d006ac4e0e1548dcebff157088e094d06
Bransblu <brandon.p.driver@gmail.com>
2022-06-16 14:58:43 -0700

    Updating GeoJSON_points.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,31 +1,31 @@
 // Add console.log to check to see if our code is working.
-console.log("working");
+//console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+//let map = L.map('mapid').setView([30, 30], 2);
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "state":"CA",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
-// Grabbing our GeoJSON data.
+
+/* // Grabbing our GeoJSON data.
 L.geoJSON(sanFranAirport, {
     onEachFeature: function(feature, layer) {
         console.log(layer);
@@ -33,9 +33,8 @@ L.geoJSON(sanFranAirport, {
         //("<h2>" + feature.properties.name + feature.properties.city + ", " + feature.properties.state + "</h2>");
     }
 
-    }).addTo(map);
+    }).addTo(map); */
 
-let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 /* cityData.forEach(function(city) {
@@ -58,11 +57,50 @@ const navDayStyle = 'navigation-day-v1';
 const navNightStyle = 'navigation-night-v1';
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let darkLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data @ <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY,
-    id: outdoorStyle
+    id: darkStyle
 });
+
+let streetsLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data @ <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+    id: streetsStyle
+});
+
+let baseMaps = {
+    Dark: darkLayer,
+    Streets: streetsLayer
+};
+
+// Create the map object with center and zoom level.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streetsLayer]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/Bransblu/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing JSON data
+d3.json(airportData).then(function(data) {
+    console.log(data);
+
+    L.geoJSON(data, {
+        onEachFeature: function(feature, layer) {
+            console.log(layer);
+            layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa +
+            "</h3><hr><p>" + feature.properties.name + "</p>");
+
+        }
+    }).addTo(map);
+});
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
